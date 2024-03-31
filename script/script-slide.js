967cbeb5e085ee286cccbb162a9a54c37282fe30
nandauforians <nanda.uforians@gmail.com>
2024-03-31 15:51:41 +0530

    SLIDES
    - corrections to video 322
    - introduction of slides and related images

--- script/script-slide.js
+++ script/script-slide.js
@@ -1,20 +1,19 @@
+var slideIndex = 0;
+showSlides();
 
-    var slideIndex = 0;
-    showSlides();
-
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}
-        slides[slideIndex-1].style.display = "block";
-        setTimeout(showSlides, 5000); // Change image every 5 seconds
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
 
-    // Next/previous controls
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
